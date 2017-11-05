a9c64f4ff7a82f435a02337b1bea082c90e087f5
Austin Ly <ahl5hm@virginia.edu>
2017-11-05 01:42:49 -0400

    Refactored frontend and cleaned files

--- RegisterUser.php
+++ RegisterUser.php
@@ -14,11 +14,11 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
   <link rel="stylesheet" href="assets/css/main.css" />
   <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
   <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
 
     //Load composer's autoloader
-    require 'vendor/autoload.php';?>
+  require 'vendor/autoload.php';?>
 </head>
 <body class="homepage">
   <div id="page-wrapper">
@@ -125,9 +125,9 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
                     $mail->addEmbeddedImage('images/BandanAirThumbnail.png', 'Logo');
                     $mail->Subject = 'Bandanair Registration Success';
                     $mail->Body    = '<h2>Congratulations ' . $name . '!</h2>
-                                      <h3>You have registered an account with Bandanair.</h3>
-                                      <h3>Please log in using your username:</h3><h1>' . $username . '</h1><br/><br/>
-                                      <img src="cid:Logo"/>';
+                    <h3>You have registered an account with Bandanair.</h3>
+                    <h3>Please log in using your username:</h3><h1>' . $username . '</h1><br/><br/>
+                    <img src="cid:Logo"/>';
                     $mail->send();
                   }
                 }
@@ -140,68 +140,68 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
         </div>
 
         <!-- Footer -->
-  <div id="footer-wrapper">
-    <footer id="footer" class="container">
-      <div class="row">
-        <div class="3u 6u(medium) 12u$(small)">
-          <a href="index.html"><img src="images/BandanAir1.png" width="252" height="156.6" alt="" /></a>
-        </div>
-        <div class="3u 6u$(medium) 12u$(small)">
+        <div id="footer-wrapper">
+          <footer id="footer" class="container">
+            <div class="row">
+              <div class="3u 6u(medium) 12u$(small)">
+                <a href="index.html"><img src="images/BandanAir1.png" width="252" height="156.6" alt="" /></a>
+              </div>
+              <div class="3u 6u$(medium) 12u$(small)">
 
-          <!-- Links -->
-          <section class="widget links">
-            <h3>Affiliated Events</h3>
-            <ul class="style2">
-              <li><a href="https://ultramusicfestival.com/">Ultra Music Festival</a></li>
-              <li><a href="https://lasvegas.electricdaisycarnival.com/">EDC Music Festival</a></li>
-              <li><a href="http://movement.us/">Movement Music Festival</a></li>
-            </ul>
-          </section>
+                <!-- Links -->
+                <section class="widget links">
+                  <h3>Affiliated Events</h3>
+                  <ul class="style2">
+                    <li><a href="https://ultramusicfestival.com/">Ultra Music Festival</a></li>
+                    <li><a href="https://lasvegas.electricdaisycarnival.com/">EDC Music Festival</a></li>
+                    <li><a href="http://movement.us/">Movement Music Festival</a></li>
+                  </ul>
+                </section>
 
-        </div>
-        <div class="3u 6u(medium) 12u$(small)">
+              </div>
+              <div class="3u 6u(medium) 12u$(small)">
 
-          <!-- Links -->
-          <section class="widget links">
-            <h3>Sitemap</h3>
-            <ul class="style2">
-              <li><a href="index.html">Home</a></li>
-              <li><a href="purchase.html">Purchase</a></li>
-              <li><a href="about.html">About Us</a></li>
-              <li><a href="founders.html">Founders</a></li>
-              <li><a href="sign-up.html">Sign Up</a></li>
-              <li><a href="login.html">Login</a></li>
-            </ul>
-          </section>
+                <!-- Links -->
+                <section class="widget links">
+                  <h3>Sitemap</h3>
+                  <ul class="style2">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="purchase.html">Purchase</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="founders.html">Founders</a></li>
+                    <li><a href="sign-up.html">Sign Up</a></li>
+                    <li><a href="login.html">Login</a></li>
+                  </ul>
+                </section>
 
-        </div>
-        <div class="3u 6u$(medium) 12u$(small)">
+              </div>
+              <div class="3u 6u$(medium) 12u$(small)">
 
-          <!-- Contact -->
-          <section class="widget contact last">
-            <h3>Contact Us</h3>
-            <ul>
-              <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-              <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-              <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-              <li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
-            </ul>
-            <p>1900 Jefferson Park Avenue Apt 6<br />
-              Charlottesville, VA 22903<br />
-            (571) 201-9700</p>
-          </section>
+                <!-- Contact -->
+                <section class="widget contact last">
+                  <h3>Contact Us</h3>
+                  <ul>
+                    <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+                  </ul>
+                  <p>1900 Jefferson Park Avenue Apt 6<br />
+                    Charlottesville, VA 22903<br />
+                  (571) 201-9700</p>
+                </section>
 
-        </div>
-      </div>
-    </footer>
-        <!-- Scripts -->
+              </div>
+            </div>
+          </footer>
+          <!-- Scripts -->
 
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/jquery.dropotron.min.js"></script>
-        <script src="assets/js/skel.min.js"></script>
-        <script src="assets/js/util.js"></script>
-        <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-        <script src="assets/js/main.js"></script>
+          <script src="assets/js/jquery.min.js"></script>
+          <script src="assets/js/jquery.dropotron.min.js"></script>
+          <script src="assets/js/skel.min.js"></script>
+          <script src="assets/js/util.js"></script>
+          <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+          <script src="assets/js/main.js"></script>
 
-      </body>
-      </html>
+        </body>
+        </html>
--- UserRegistered.php
+++ UserRegistered.php
@@ -34,68 +34,68 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 						<nav id="nav">
 							<ul>
 								<li><a href="index.html">Home</a></li>
-            					<li><a href="purchase.html">Purchase</a></li>
-					            <li><a href="about.html">About Us</a></li>
-					            <li><a href="sign-up.html">Sign Up</a></li>
-					            <li class="login"><a href="login.html">Login</a></li>
-			</ul>
-		</nav>
+								<li><a href="purchase.html">Purchase</a></li>
+								<li><a href="about.html">About Us</a></li>
+								<li><a href="sign-up.html">Sign Up</a></li>
+								<li class="login"><a href="login.html">Login</a></li>
+							</ul>
+						</nav>
 
-	</header>
-</div>
+					</header>
+				</div>
 
-<!-- Banner -->
-<div id="banner-wrapper" style="margin-top: -30px;">
-	<div id="banner" class="box container">
-		<div class="row">
-			<div class="7u 12u(medium)">
-        <h2> Successfully Registered! </h2>
-				<p style="font-size: 35px"> <?php
+				<!-- Banner -->
+				<div id="banner-wrapper" style="margin-top: -30px;">
+					<div id="banner" class="box container">
+						<div class="row">
+							<div class="7u 12u(medium)">
+								<h2> Successfully Registered! </h2>
+								<p style="font-size: 35px"> <?php
            include_once("./library.php"); // To connect to the database
            $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
            // Check connection
            if (mysqli_connect_errno())
            {
-            echo "Failed to connect to MySQL: " . mysqli_connect_error();
-            }
-            $hashed_password = null;
+           	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+           }
+           $hashed_password = null;
             // Form the SQL query (an INSERT query)
-            $name = $_POST['form_name'];
-            $email = $_POST['form_email'];
-            $address = $_POST['form_address'];
-            $city = $_POST['form_city'];
-            $state = $_POST['form_state'];
-            $zipcode = $_POST['form_zipcode'];
-            $phone= $_POST['form_phone'];
-            $birthday = $_POST['form_birthday'];
-            $username = $_POST['form_username'];
-            $password = $_POST['form_password'];
-            $hashed_password= hash('sha256',$password);
+           $name = $_POST['form_name'];
+           $email = $_POST['form_email'];
+           $address = $_POST['form_address'];
+           $city = $_POST['form_city'];
+           $state = $_POST['form_state'];
+           $zipcode = $_POST['form_zipcode'];
+           $phone= $_POST['form_phone'];
+           $birthday = $_POST['form_birthday'];
+           $username = $_POST['form_username'];
+           $password = $_POST['form_password'];
+           $hashed_password= hash('sha256',$password);
             // session_start();
             // $_SESSION["Email"] = $email;
-            $sql="INSERT INTO registeredUsers (name, email, address, city, state, zipcode,phone,birthday,username, password)
-            VALUES  ('$name', '$email', '$address', '$city', '$state', '$zipcode','$phone','$birthday','$username',
-              '$hashed_password')";
-          if (!mysqli_query($con,$sql))
-          {
-            die('Error: ' . mysqli_error($con));
-          }
-          else
-          {
-            echo "Welcome $name. You are registered as $username.";
-          }
-          mysqli_close($con);
-          ?>
-        </p>
-			</div>
-			<div class="5u 12u(medium)">
-				<ul>
-					<li><a href="#" class="button big icon fa-arrow-circle-right">View your Profile</a></li>
-					<li><a href="#" class="button alt big icon">Search Coupons</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
+           $sql="INSERT INTO registeredUsers (name, email, address, city, state, zipcode,phone,birthday,username, password)
+           VALUES  ('$name', '$email', '$address', '$city', '$state', '$zipcode','$phone','$birthday','$username',
+           '$hashed_password')";
+           if (!mysqli_query($con,$sql))
+           {
+           	die('Error: ' . mysqli_error($con));
+           }
+           else
+           {
+           	echo "Welcome $name. You are registered as $username.";
+           }
+           mysqli_close($con);
+           ?>
+       </p>
+   </div>
+   <div class="5u 12u(medium)">
+   	<ul>
+   		<li><a href="#" class="button big icon fa-arrow-circle-right">View your Profile</a></li>
+   		<li><a href="#" class="button alt big icon">Search Coupons</a></li>
+   	</ul>
+   </div>
+</div>
+</div>
 </div>
 <!-- Footer -->
 <div id="footer-wrapper" style="margin-top: -45px; margin-bottom: -20px">
@@ -116,9 +116,9 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 				<!-- Links -->
 				<section class="widget contact">
 					<h3>GitHub</h3>
-						<ul>
-							<li><a href="http://github.com/apn2my" class="icon fa-github" ><span class="label"></span></a></li>
-						</ul>
+					<ul>
+						<li><a href="http://github.com/apn2my" class="icon fa-github" ><span class="label"></span></a></li>
+					</ul>
 				</section>
 
 			</div>
@@ -127,9 +127,9 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 				<!-- Links -->
 				<section class="widget contact">
 					<h3>University</h3>
-						<ul>
-							<li><a href="http://virginia.edu" class="icon fa-graduation-cap" ><span class="label"></span></a></li>
-						</ul>
+					<ul>
+						<li><a href="http://virginia.edu" class="icon fa-graduation-cap" ><span class="label"></span></a></li>
+					</ul>
 				</section>
 			</div>
 
@@ -139,21 +139,21 @@ Free for personal and commercial use under the CCA 3.0 license (html5up.net/lice
 					<h3>Address</h3>
 					<p>85 Engineer's Way, Rice Hall<br />
 						University of Virginia<br/>
-						Charlottesville, VA 22903</p>
-					</section>
+					Charlottesville, VA 22903</p>
+				</section>
+			</div>
+		</div>
+		<div class="row" style="margin-bottom: -200px;">
+			<div class="12u" style="margin-top: -80px;">
+				<div id="copyright">
+					<ul class="menu">
+						<li>&copy; CS4753 Fall 2017. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
 				</div>
 			</div>
-			<div class="row" style="margin-bottom: -200px;">
-				<div class="12u" style="margin-top: -80px;">
-					<div id="copyright">
-						<ul class="menu">
-							<li>&copy; CS4753 Fall 2017. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</footer>
-	</div>
+		</div>
+	</footer>
+</div>
 
 </div>
 
